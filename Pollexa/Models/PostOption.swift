0cc0feaa158571ba466c2d77e1644ade5d3c8650
Cem Uğurlu <61113120+cemugurlu@users.noreply.github.com>
2024-06-10 19:17:52 +0300

    percentage added for the model extension

--- Pollexa/Models/PostOption.swift
+++ Pollexa/Models/PostOption.swift
@@ -11,16 +11,19 @@ extension Post {
     struct Option: Decodable {
         let id: String
         let image: UIImage
+        let percentage: Int
         
         enum CodingKeys: String, CodingKey {
             case id
             case imageName
+            case percentage
         }
         
         init(from decoder: Decoder) throws {
             let container = try decoder.container(keyedBy: CodingKeys.self)
             id = try container.decode(String.self, forKey: .id)
             let imageName = try container.decode(String.self, forKey: .imageName)
+            percentage = try container.decode(Int.self, forKey: .percentage)
             if let image = UIImage(named: imageName) {
                 self.image = image
             } else {
